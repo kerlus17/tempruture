5c525545a4ba56da79d49503df7a7c23e69a86a8
kerlus17 <kokokeronageh@gmail.com>
2025-02-21 08:28:02 -0800

    Add files via upload

---
+++ test 2.py
@@ -0,0 +1,13 @@
+celesious= 20
+
+if celesious >= 30:
+    print("It's a hot day. Stay hydrated!")
+
+elif 20<= celesious <=29:
+    print("It's a warm day. Enjoy the weather!")
+
+elif 10<= celesious <=19:
+    print("It's a cool day. Wear a jacket!")
+
+elif  celesious <10:
+    print("It's a cold day. Stay warm!" )
